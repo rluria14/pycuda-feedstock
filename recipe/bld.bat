71edb660321f92705b9631c3825460f3b83b2e18
rluria14 <ryanlur14@gmail.com>
2021-05-07 14:27:37 -0400

    try setting cuda lib path

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,9 @@
-set CUDA_INC_PATH=%CUDA_PATH%\include
-if errorlevel 1 exit 1
+echo CUDA_HOME: %CUDA_HOME%
+echo CUDA_PATH: %CUDA_PATH%
+
+set CUDA_PATH=C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v%cuda_compiler_version%
+set CUDA_LIB_PATH=%CUDA_PATH%\lib
+
 
 %PYTHON% configure.py --enable-shared
 if errorlevel 1 exit 1
