e0781b455fcc1e083daf99f642898b36ce6b2e37
rluria14 <ryanlur14@gmail.com>
2021-05-07 15:21:17 -0400

    find cuda.lib

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,7 +1,8 @@
 echo CUDA_HOME: %CUDA_HOME%
 echo CUDA_PATH: %CUDA_PATH%
+for /r %i in (cuda.lib) do echo  %~ti %~zi %i
 
-set CUDA_LIB_PATH=%CUDA_PATH%\lib\x64
+set CUDA_LIB_PATH=%CUDA_PATH%\lib64
 
 
 %PYTHON% configure.py --enable-shared
